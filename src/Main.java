2b625f1ff540ca342588467b4c704ed301bb114d
aldinalj <aldinalj@gmail.com>
2023-09-29 14:08:06 +0200

    Clean slate

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       /* int number = 5;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Type something to crash the program");
-        scanner.nextLine();
-        System.out.println(number/0); */
-
-        int[] numbers = {0, 100, 1000, 2000};
-        System.out.println("Before crash");
-
-        try {
-            // Try to execute code
-            System.out.println("Inside - TRY");
-            System.out.println(2/0);
-        } catch (Exception error) {
-            // Catch error if it FAILS
-            System.out.println("Inside - CATCH");
-            /* Fångar upp problemet och loggar ut hela error problemet men inte slay för att publicera pga tex hacking
-            error.printStackTrace(); */
-        }
-
-        System.out.println("After crash");
-
         }
     }
