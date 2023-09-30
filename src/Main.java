c66782adbf4cc1959defa8cf7745304b026b2aae
aldinalj <aldinalj@gmail.com>
2023-09-30 11:57:16 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
+      /* TODO - tep by Step with METHODS
+      *   How should we structure out project?
+      *   How should one think?
+      *   How should one start?
+      *   How should one end?
+      * */
+
+        /* TODO
+        *   Order product
+        *   Pay for product
+        *   Ship product
+        *   End
+        *  */
+
+        // #1 orderProduct()
+        // #2 payForProduct()
+        // #3 shipProduct()
+        // #4 sendConfirmationEmail()
+
+        // Initialize project
+        // Setup Variables
+        // setupGame()
+            // inputPlayerName()
+            // inputAmountOfDice()
+        
+        // player
+            // int score;
+            // throwDice()
+
+        // scoreLogic
+            // determineWinner()
+            // calculateScore()
+           // throwDice()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
